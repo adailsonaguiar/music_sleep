1eb85f37f853adcefdcafa0236b645b32345a5d2
adailsonaguiar <adailsonacj@live.com>
2024-03-16 09:47:53 -0300

    increase & decrease button

--- app/src/main/java/com/example/musicsleep/MainActivity.kt
+++ app/src/main/java/com/example/musicsleep/MainActivity.kt
@@ -8,38 +8,55 @@ import android.view.KeyEvent
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var decreaseBtn: Button
+    private lateinit var startBtn: Button
+    private lateinit var decreaseBtn: FloatingActionButton
+    private lateinit var increaseBtn: FloatingActionButton
     private lateinit var remainingTime: TextView
     private lateinit var audioManager: AudioManager
     private var timer: CountDownTimer? = null
     private var oneMinute: Long = 60000
-    private var remainingTimeMillis: Long = oneMinute * 30
+    private var minuteMultiple = 1
+    private var remainingTimeMillis: Long = oneMinute * minuteMultiple
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        decreaseBtn = findViewById(R.id.decrease)
+        startBtn = findViewById(R.id.start)
         remainingTime = findViewById(R.id.remainingTime)
+        decreaseBtn = findViewById(R.id.decrease)
+        increaseBtn= findViewById(R.id.increase)
 
         audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
 
-        decreaseBtn.setOnClickListener {
-            iniciarTemporizador()
-            Toast.makeText(this, "Música pausará em 30 minutos", Toast.LENGTH_SHORT).show()
+        startBtn.setOnClickListener {
+            if (timer != null) {
+                resetCounter()
+                timer?.cancel() // Cancela o temporizador existente se houver
+            }
+            initializerTimer()
+        }
 
+        decreaseBtn.setOnClickListener{
+            if(minuteMultiple> 1) {
+                minuteMultiple = minuteMultiple - 3
+                resetCounter()
+            }
+        }
+        increaseBtn.setOnClickListener{
+            minuteMultiple = minuteMultiple + 3
+            resetCounter()
         }
     }
 
-    private fun iniciarTemporizador() {
-        timer?.cancel()
-
+    private fun initializerTimer(){
         timer = object : CountDownTimer(remainingTimeMillis, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 remainingTimeMillis = millisUntilFinished
-                atualizarTextoTempoRestante()
+                updateRemainingTime()
             }
 
 
@@ -52,15 +69,20 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "Música pausada", Toast.LENGTH_SHORT).show()
             }
         }
-
         timer?.start()
     }
 
-    private fun atualizarTextoTempoRestante() {
+    private fun updateRemainingTime() {
         val min = remainingTimeMillis / 60000
         val sec = (remainingTimeMillis % 60000) / 1000
         val timeText = String.format("%d:%02d", min, sec)
         remainingTime.text = timeText
+    }
+
+    private fun resetCounter() {
+        timer?.cancel()
+        remainingTimeMillis = oneMinute * minuteMultiple
+        updateRemainingTime()
     }
 
     override fun onDestroy() {
